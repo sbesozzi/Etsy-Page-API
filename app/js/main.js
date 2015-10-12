95ef79c8821cab8507a6a3fe76a47c74020687de
Sarah Besozzi <sarah.besozzi@gmail.com>
2015-10-12 16:31:33 -0400

    Edited main.js template

--- app/js/main.js
+++ app/js/main.js
@@ -2,37 +2,22 @@
 
 (function () {
 
-  // var firstItem = etsy.results[0];
-
   var templateString = $('#itemTemplate').text();
-
-  // var templateString = ' <div><p><%= Images[0] %></p><p><%= title %></p> <p><%= Shop.shop_name %></p><p><%= "$"+price+currency_code %></p></div>';
-
-  // <div>
-  //   <p>
-  //   <%= Images[0] %>
-  //   </p>
-  // </div>
-
-  // <div>
-  //   <p>
-  //   <%= title %>
-  //   </p>
-  //   <p>
-  //   <%= Shop.shop_name %>
-  //   </p>
-  //   <p>
-  //   <%= "$"+price+currency_code %>
-  //   </p>
-  // </div>
-
-  // Call template function to call out string
   var templateFunction = _.template(templateString);
 
-  _.each(etsy.results, function (item) {
+  var etsyURL = 'https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=shibori+pillow&includes=Images,Shop';
 
-    var itemHTML = templateFunction(item);
+  $.ajax({
+    url: etsyURL,
+    dataType: 'jsonp',
+    method: 'get'
+  }).then(function (response) {
+    console.log(response);
 
-    $('.item-results').append(itemHTML);
+    _.each(response.results, function (item) {
+
+      var itemHTML = templateFunction(item);
+      $('.item-results').append(itemHTML);
+    });
   });
 })();
--- src/main.js
+++ src/main.js
@@ -1,41 +1,25 @@
 (function () {
 
-// var firstItem = etsy.results[0];
-
 var templateString = $('#itemTemplate').text();
-
-// var templateString = ' <div><p><%= Images[0] %></p><p><%= title %></p> <p><%= Shop.shop_name %></p><p><%= "$"+price+currency_code %></p></div>';
-
-// <div>
-//   <p>
-//   <%= Images[0] %>
-//   </p>
-// </div>
-
-// <div>
-//   <p>
-//   <%= title %>
-//   </p> 
-//   <p>
-//   <%= Shop.shop_name %>
-//   </p>
-//   <p>
-//   <%= "$"+price+currency_code %>
-//   </p>
-// </div>
-
-// Call template function to call out string 
 var templateFunction = _.template(templateString);
 
-_.each(etsy.results, function (item) {
+
+let etsyURL = 'https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=shibori+pillow&includes=Images,Shop';
+
+$.ajax({
+  url: etsyURL,
+  dataType: 'jsonp',
+  method: 'get'
+}).then (function (response) {
+  console.log(response);
+ 
 
 
-  
-  var itemHTML = templateFunction(item);
-  
-  $('.item-results').append(itemHTML);
+  _.each(response.results, function (item) {
+    
+    var itemHTML = templateFunction(item);
+    $('.item-results').append(itemHTML);
+  });
+ });
 
-});
-
-  
 }());
